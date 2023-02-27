1aceec1350d955b023a59e2b9a51100734c6fd34
Sirisha Veeralapalli <Sirisha.V@WinWire.com>
2023-02-27 13:12:12 +0530

    assignment3 Login initial commit

---
+++ Login/Login/Login.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace Login
+{
+    public partial class Login : ContentPage
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+        //Login
+        async void Button_Clicked(System.Object sender, System.EventArgs e)
+        {
+            if (string.IsNullOrEmpty(username.Text.ToString()))
+            {
+                DisplayAlert("Alert", "Please enter username","OK");
+                return;
+            }
+            if (string.IsNullOrEmpty(password.Text.ToString()))
+            {
+                DisplayAlert("Alert", "Please enter password", "OK");
+                return;
+            }
+            if(username.Text.Equals("admin") && password.Text.Equals("admin"))
+            {
+                await Navigation.PushAsync(new Home());
+            }
+            else
+            {
+                await DisplayAlert("Alert", "Please check your credentials", "OK");
+                return;
+
+            }
+        }
+        //Register
+        void Button_Clicked_1(System.Object sender, System.EventArgs e)
+        {
+            Navigation.PushAsync(new RegisterPage());
+        }
+
+        void Button_Clicked_2(System.Object sender, System.EventArgs e)
+        {
+            if (string.IsNullOrEmpty(username.Text))
+            {
+                DisplayAlert("Alert", "Please enter email", "OK");
+                return;
+            }
+            else
+            {
+                DisplayAlert("Alert", "Password is sent to your email successfully", "OK");
+            }
+        }
+    }
+}
+
---
+++ Login/Login/RegisterPage.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace Login
+{
+    public partial class RegisterPage : ContentPage
+    {
+        public RegisterPage()
+        {
+            InitializeComponent();
+        }
+
+        async void Button_Clicked(System.Object sender, System.EventArgs e)
+        {
+            if (string.IsNullOrEmpty(firstName.Text))
+            {
+                DisplayAlert("Alert", "Please enter first name", "OK");
+                return;
+            }
+            if (string.IsNullOrEmpty(lastName.Text))
+            {
+                DisplayAlert("Alert", "Please enter first name", "OK");
+                return;
+            }
+            if (string.IsNullOrEmpty(email.Text))
+            {
+                DisplayAlert("Alert", "Please enter first name", "OK");
+                return;
+            }
+            if (string.IsNullOrEmpty(phone.Text))
+            {
+                DisplayAlert("Alert", "Please enter first name", "OK");
+                return;
+            }
+            var result = await DisplayAlert("Alert", "Are you sure you want to register with these details","Yes","No");
+            if (result)
+            {
+                await DisplayAlert("Alert", "You registered successfully", "OK");
+                await Navigation.PushAsync(new Login());
+            }
+            else
+            {
+                return;
+            }
+        }
+    }
+}
+
